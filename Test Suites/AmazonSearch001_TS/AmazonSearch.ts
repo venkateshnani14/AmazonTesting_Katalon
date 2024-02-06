88b1754c093ed3d4be3fdc2ce79c2ee0a86fec70
venkateshnani <venkateshkasani14@gmail.com>
2024-02-06 21:47:18 +0530

    Search bar testing(data driven) is done.

---
+++ Test Suites/AmazonSearch001_TS/AmazonSearch.ts
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>AmazonSearch</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e1a64084-5976-4517-8e2f-47c228c03b89</testSuiteGuid>
+   <testCaseLink>
+      <guid>2a78a9bf-d850-4bb3-bcba-a77eb6ddf1c7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AmazonLogin/TC_AmazonSearch001</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>5c1fb981-1bcf-4c71-830b-0bfd3bf2629f</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/AmazonSearch/AmazonSearch001_DATA</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>5c1fb981-1bcf-4c71-830b-0bfd3bf2629f</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>SearchInput</value>
+         <variableId>948216a3-d856-44dc-ba64-c01a909cc0b6</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
